7999d2c41438097501ca27d55373f23630325d60
K_Boan <54317630+dongho108@users.noreply.github.com>
2020-07-27 16:08:18 +0900

    nodejs

---
+++ syntax/template.js
@@ -0,0 +1,12 @@
+var name = 'k8805';
+
+var letter = 'Dear ' +name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, ' +name+' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ' +name+' aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ' +name;
+
+var letter = `Dear ${name}
+
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, ${name} sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ${1+1} aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.${name}`;
+
+console.log(letter);
+
+var a = '1';
---
+++ syntax/variable.js
@@ -0,0 +1,5 @@
+var a = 2;
+console.log(a);
+a = 5;
+console.log(a);
+//1 = 2;
